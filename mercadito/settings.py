a5cde8d4354d2ebc178c783a7084aaaed7903ad8
mtom92 <torresmoranm@gmail.com>
2019-06-17 14:56:02 -0700

    changes for deploy

--- mercadito/settings.py
+++ mercadito/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from django.utils.translation import ugettext_lazy as _
-
-# THIS IS FOR DEPLOYMENT
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -138,26 +136,26 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # # this is directory name where collectstatic files command will put your app level static files
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #
 # # this defines the url for static files
 # # eg: base-url.com/static/your-js-file.js
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 #
 # # this is directory paths where you have to put your project level static files
 # # you can put multiple folders here
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'basics/static'),
-    os.path.join(BASE_DIR, 'basics'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'basics/static'),
+#     os.path.join(BASE_DIR, 'basics'),
+# )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
